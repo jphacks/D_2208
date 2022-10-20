8183f24f201b2610f1d46989fa7a255b6080a9cd
kyochn <is0586vi@ed.ritsumei.ac.jp>
2022-10-20 21:19:58 +0900

    set module alias

--- app/src/main/pointer/vite.config.ts
+++ app/src/main/pointer/vite.config.ts
@@ -1,8 +1,14 @@
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.join(__dirname, "src"),
+    },
+  },
   server: {
     proxy: {
       '/api': {
