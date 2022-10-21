6a8c4ba370150ebcdc234713f2b141e1c9743e47
Hoshi Ran <ureshinostyle350@gmail.com>
2022-10-22 02:12:36 +0900

    feat: desktop stomp client を導入

--- desktop/electron/main/stomp.ts
+++ desktop/electron/main/stomp.ts
@@ -1,11 +1,12 @@
 import { Client } from "@stomp/stompjs";
-import { WebSocket } from "ws";
-
-Object.assign(global, { WebSocket });
+import SockJS from "sockjs-client";
 
 const brokerURL =
   process.env["NODE_ENV"] === "development"
-    ? "ws://localhost:8080/ws"
-    : "wss://smartpointer.abelab.dev/ws";
+    ? "http://localhost:8080/ws"
+    : "https://smartpointer.abelab.dev/ws";
 
-export const stompClient = new Client({ brokerURL });
+export const stompClient = new Client({
+  webSocketFactory: () => new SockJS(brokerURL),
+  debug: console.log,
+});
