4b93529e12b0eb419f27a0166a605b5597d0c476
averak <abe12@mccc.jp>
2022-10-21 20:37:52 +0900

    [feat] ルーム削除APIを実装

---
+++ app/src/test/groovy/dev/abelab/smartpointer/usecase/DeleteRoomUseCase_UT.groovy
@@ -0,0 +1,43 @@
+package dev.abelab.smartpointer.usecase
+
+import dev.abelab.smartpointer.domain.model.RoomModel
+import dev.abelab.smartpointer.exception.BaseException
+import dev.abelab.smartpointer.exception.ErrorCode
+import dev.abelab.smartpointer.exception.NotFoundException
+import dev.abelab.smartpointer.helper.RandomHelper
+import org.springframework.beans.factory.annotation.Autowired
+
+/**
+ * DeleteRoomUseCaseの単体テスト
+ */
+class DeleteRoomUseCase_UT extends AbstractUseCase_UT {
+
+    @Autowired
+    DeleteRoomUseCase sut
+
+    def "handle: ルームを削除する"() {
+        given:
+        final room = RandomHelper.mock(RoomModel)
+
+        when:
+        this.sut.handle(room.id)
+
+        then:
+        1 * this.roomRepository.existsById(room.id) >> true
+        1 * this.roomRepository.deleteById(room.id)
+    }
+
+    def "handle: ルームが存在しない場合は404エラー"() {
+        given:
+        final room = RandomHelper.mock(RoomModel)
+
+        when:
+        this.sut.handle(room.id)
+
+        then:
+        1 * this.roomRepository.existsById(room.id) >> false
+        final BaseException exception = thrown()
+        verifyException(exception, new NotFoundException(ErrorCode.NOT_FOUND_ROOM))
+    }
+
+}
