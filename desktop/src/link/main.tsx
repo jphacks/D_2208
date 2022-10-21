e4b97baa7e97e2d9c488b1cef41c2abe9621eeca
Hoshi Ran <ureshinostyle350@gmail.com>
2022-10-21 18:13:52 +0900

    add: theme

--- desktop/src/link/main.tsx
+++ desktop/src/link/main.tsx
@@ -3,12 +3,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { App } from "./App";
+import { theme } from "./theme";
 
 // HTML で定義されているので disable
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </StrictMode>
