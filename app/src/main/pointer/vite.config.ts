8a4f7d2b6eed56976c20d8200cff22a660b1bbfb
kyochn <is0586vi@ed.ritsumei.ac.jp>
2022-10-22 03:51:29 +0900

    serve HTTPS in dev

--- app/src/main/pointer/vite.config.ts
+++ app/src/main/pointer/vite.config.ts
@@ -1,9 +1,10 @@
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   resolve: {
     alias: {
       "@": path.join(__dirname, "src"),
