6b5edf3fa76951475e0cdf8b0dbf9abc27decc14
Hoshi Ran <ureshinostyle350@gmail.com>
2022-10-21 21:43:27 +0900

    feat: desktop の stomp client を追加

--- desktop/electron/main/app.ts
+++ desktop/electron/main/app.ts
@@ -4,9 +4,13 @@ import { AppState } from "@/AppState";
 import { showInviteLinkWindow } from "@/link";
 import { createTray } from "@/menu/tray";
 import { createRoom } from "@/room";
+import { stompClient } from "@/stomp";
 
 app.once("ready", () => {
   const appState = new AppState();
+
+  stompClient.activate();
+
   createTray({
     appState,
     handleClickCreateRoom: createRoom(appState),
