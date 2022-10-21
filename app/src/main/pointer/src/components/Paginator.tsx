b4db89a05ef4dbde00dab87cde320e238d65a087
kyochn <is0586vi@ed.ritsumei.ac.jp>
2022-10-21 18:01:04 +0900

    fix UI

--- app/src/main/pointer/src/components/Paginator.tsx
+++ app/src/main/pointer/src/components/Paginator.tsx
@@ -1,21 +1,23 @@
-import { IconButton, Icon, Flex } from "@chakra-ui/react";
+import { IconButton, Icon, Flex, VStack, Heading } from "@chakra-ui/react";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
 import { FC } from "react";
 
 export const Paginator: FC = () => {
   return (
-    <Flex justify="center" gap={12}>
+    <Flex justify="center" gap={12} w="full">
       {[
-        { icon: ChevronLeftIcon, label: "1ページ戻す" },
-        { icon: ChevronRightIcon, label: "1ページ進める" },
-      ].map(({ icon, label }, index) => (
-        <IconButton
-          key={`${index}`}
-          aria-label={label}
-          flexGrow={1}
-          height={24}
-          icon={<Icon width={14} height={14} as={icon} />}
-        />
+        { icon: ChevronLeftIcon, label: "1ページ戻す", text: "前へ" },
+        { icon: ChevronRightIcon, label: "1ページ進める", text: "次へ" },
+      ].map(({ icon, label, text }, index) => (
+        <VStack key={`${index}`} flexGrow="1">
+          <Heading fontSize="xl">{text}</Heading>
+          <IconButton
+            aria-label={label}
+            w="full"
+            height={24}
+            icon={<Icon width={14} height={14} as={icon} />}
+          />
+        </VStack>
       ))}
     </Flex>
   );
