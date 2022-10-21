c11538b68efc3cb1421b0eb277da1e720f2a6230
kyochan <66947667+kyochn@users.noreply.github.com>
2022-10-22 01:24:00 +0900

    WSした (#131)
    * setup stompClient
    
    * f**ckin sockjs
    
    * fix format

--- app/src/main/pointer/src/stomp.ts
+++ app/src/main/pointer/src/stomp.ts
@@ -1,7 +1,6 @@
 import { Client } from "@stomp/stompjs";
+import SockJS from "sockjs-client/dist/sockjs";
 
 export const stompClient = new Client({
-  brokerURL: `ws${location.protocol === "https:" ? "s" : ""}://${
-    location.host
-  }/ws`,
+  webSocketFactory: () => new SockJS(`/ws`),
 });
