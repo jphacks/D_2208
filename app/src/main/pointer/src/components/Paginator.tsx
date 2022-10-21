de836f17721d1367e746b65ddefb9835fa73850c
kyochn <is0586vi@ed.ritsumei.ac.jp>
2022-10-21 22:54:13 +0900

    publish pagination control

--- app/src/main/pointer/src/components/Paginator.tsx
+++ app/src/main/pointer/src/components/Paginator.tsx
@@ -2,6 +2,7 @@ import { IconButton, Icon, Flex, VStack, Heading } from "@chakra-ui/react";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
 import { FC } from "react";
 
+import { client } from "@/stomp";
 import { AuthData } from "@/types/AuthData";
 
 type Props = {
@@ -9,6 +10,18 @@ type Props = {
 };
 
 export const Paginator: FC<Props> = ({ authData }) => {
+  const goNext = () =>
+    client.publish({
+      destination: `/app/rooms/${authData.roomId}/slides/next`,
+      body: JSON.stringify({ room_id: authData.roomId }),
+    });
+
+  const goPrevious = () =>
+    client.publish({
+      destination: `/app/rooms/${authData.roomId}/slides/previous`,
+      body: JSON.stringify({ room_id: authData.roomId }),
+    });
+
   return (
     <Flex justify="center" gap={12} w="full">
       {[
@@ -16,20 +29,22 @@ export const Paginator: FC<Props> = ({ authData }) => {
           icon: ChevronLeftIcon,
           label: "1ページ戻す",
           text: "前へ",
+          onClick: goPrevious,
         },
         {
           icon: ChevronRightIcon,
           label: "1ページ進める",
           text: "次へ",
+          onClick: goNext,
         },
-      ].map(({ icon, label, text }, index) => (
+      ].map(({ icon, label, text, onClick }, index) => (
         <VStack key={`${index}`} flexGrow="1">
           <Heading fontSize="xl">{text}</Heading>
           <IconButton
             aria-label={label}
             w="full"
             height={24}
-            icon={<Icon width={14} height={14} as={icon} />}
+            icon={<Icon width={14} height={14} as={icon} onClick={onClick} />}
           />
         </VStack>
       ))}
