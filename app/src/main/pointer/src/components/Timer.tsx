3893b2f08df0a3805e238a50b46e1c78e40f9a1d
ygkn <2000ygkn0713@gmail.com>
2022-10-22 10:56:33 +0900

    タイマーうごくようにした

--- app/src/main/pointer/src/components/Timer.tsx
+++ app/src/main/pointer/src/components/Timer.tsx
@@ -20,20 +20,31 @@ import {
   InputLeftAddon,
   InputRightAddon,
   Heading,
+  useToast,
 } from "@chakra-ui/react";
 import { Cog6ToothIcon } from "@heroicons/react/24/solid";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
+import { stompClient } from "@/stomp";
 import { AuthData } from "@/types/AuthData";
 
 type Props = {
   authData: AuthData;
 };
 
+type FormValues = {
+  minutes: string;
+  seconds: string;
+};
+
 export const Timer: FC<Props> = ({ authData }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [state, setState] = useState<"READY" | "RUNNING">("READY");
+  const [finishTimestamp, setFinishedTimestamp] = useState<number | null>(null);
   const [noticeTime, setNoticeTime] = useState<string>("1");
+
+  const toast = useToast();
 
   const completeNoticeTime = () => {
     if (noticeTime === "") {
@@ -42,13 +53,117 @@ export const Timer: FC<Props> = ({ authData }) => {
     onClose();
   };
 
+  const { register, handleSubmit, setValue } = useForm<FormValues>();
+
+  const onStartStop = (values: FormValues) => {
+    console.log(values);
+    switch (state) {
+      case "READY": {
+        stompClient.publish({
+          destination: `/app/rooms/${authData.roomId}/timer/start`,
+          body: JSON.stringify({
+            value: Number(values.minutes) * 60 + Number(values.seconds),
+          }),
+        });
+        return;
+      }
+      case "RUNNING": {
+        stompClient.publish({
+          destination: `/app/rooms/${authData.roomId}/timer/stop`,
+        });
+        return;
+      }
+    }
+  };
+
+  useEffect(() => {
+    stompClient.subscribe(
+      `/topic/rooms/${authData.roomId}/timer`,
+      (message) => {
+        console.log(message);
+        const { status, value, finishAt } = JSON.parse(message.body) as {
+          status: 0 | 1;
+          value: number;
+          finishAt: string;
+        };
+        switch (status) {
+          case 0: {
+            const minutes = Math.floor(value / 60);
+            const seconds = value % 60;
+            setValue("minutes", minutes.toString());
+            setValue("seconds", seconds.toString());
+            setState("READY");
+            return;
+          }
+          case 1: {
+            setFinishedTimestamp(
+              performance.now() + new Date(finishAt).valueOf() - Date.now()
+            );
+            setState("RUNNING");
+            return;
+          }
+        }
+      }
+    );
+  }, [authData.roomId, setValue]);
+
+  useEffect(() => {
+    if (finishTimestamp === null) {
+      return;
+    }
+
+    let requestId: number | null;
+
+    const tick = (now: number) => {
+      const timeStamp = finishTimestamp - now;
+
+      if (timeStamp <= 0) {
+        setValue("minutes", "0");
+        setValue("seconds", "0");
+        setState("READY");
+        toast({
+          title: "タイマーが終了しました",
+          description: "お疲れ様でした",
+          status: "success",
+          duration: 9000,
+          isClosable: true,
+        });
+        return;
+      }
+
+      const seconds = Math.floor((timeStamp / 1000) % 60);
+      const minutes = Math.floor((timeStamp / 1000 / 60) % 60);
+      setValue("minutes", minutes.toString());
+      setValue("seconds", seconds.toString());
+
+      requestId = requestAnimationFrame(tick);
+    };
+
+    if (state === "RUNNING") {
+      requestId = requestAnimationFrame(tick);
+    }
+
+    return () => {
+      if (requestId) {
+        cancelAnimationFrame(requestId);
+      }
+    };
+  }, [finishTimestamp, setValue, state]);
+
   return (
     <VStack gap={2}>
       <Heading fontSize="xl">タイマー</Heading>
-      <VStack gap={4}>
+      <VStack gap={4} as="form" onSubmit={handleSubmit(onStartStop)}>
         <Flex justify="center" align="center">
           <NumberInput defaultValue={5} min={0} size="lg" flexGrow={1}>
-            <NumberInputField textAlign="right" fontSize="5xl" height={24} />
+            <NumberInputField
+              textAlign="right"
+              fontSize="5xl"
+              height={24}
+              {...register("minutes", {
+                required: true,
+              })}
+            />
             <NumberInputStepper>
               <NumberIncrementStepper />
               <NumberDecrementStepper />
@@ -68,7 +183,14 @@ export const Timer: FC<Props> = ({ authData }) => {
             />
           </Icon>
           <NumberInput defaultValue={0} min={0} max={59} size="lg" flexGrow={1}>
-            <NumberInputField textAlign="right" fontSize="5xl" height={24} />
+            <NumberInputField
+              textAlign="right"
+              fontSize="5xl"
+              height={24}
+              {...register("seconds", {
+                required: true,
+              })}
+            />
             <NumberInputStepper>
               <NumberIncrementStepper />
               <NumberDecrementStepper />
@@ -76,58 +198,44 @@ export const Timer: FC<Props> = ({ authData }) => {
           </NumberInput>
         </Flex>
         <Flex gap={8}>
-          <Button
-            width={24}
-            disabled={state === "READY"}
-            onClick={() => setState("READY")}
-          >
-            リセット
-          </Button>
-          <Button
-            width={24}
-            onClick={() =>
-              setState((oldState) =>
-                oldState === "READY" ? "RUNNING" : "READY"
-              )
-            }
-          >
+          <Button width={24} type="submit">
             {state === "READY" ? "スタート" : "ストップ"}
           </Button>
           <Button onClick={onOpen}>
             <Icon as={Cog6ToothIcon} />
           </Button>
-          <Modal isOpen={isOpen} onClose={completeNoticeTime}>
-            <ModalOverlay />
-            <ModalContent>
-              <ModalHeader>残り時間通知</ModalHeader>
-              <ModalCloseButton />
-              <ModalBody>
-                <InputGroup>
-                  <InputLeftAddon>残り</InputLeftAddon>
-                  <NumberInput
-                    min={0}
-                    value={noticeTime}
-                    onChange={(value) => setNoticeTime(value)}
-                  >
-                    <NumberInputField />
-                    <NumberInputStepper>
-                      <NumberIncrementStepper />
-                      <NumberDecrementStepper />
-                    </NumberInputStepper>
-                  </NumberInput>
-                  <InputRightAddon>分</InputRightAddon>
-                </InputGroup>
-              </ModalBody>
-
-              <ModalFooter>
-                <Button colorScheme="blue" mr={3} onClick={completeNoticeTime}>
-                  完了
-                </Button>
-              </ModalFooter>
-            </ModalContent>
-          </Modal>
         </Flex>
       </VStack>
+      <Modal isOpen={isOpen} onClose={completeNoticeTime}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>残り時間通知</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <InputGroup>
+              <InputLeftAddon>残り</InputLeftAddon>
+              <NumberInput
+                min={0}
+                value={noticeTime}
+                onChange={(value) => setNoticeTime(value)}
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+              <InputRightAddon>分</InputRightAddon>
+            </InputGroup>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={completeNoticeTime}>
+              完了
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </VStack>
   );
 };
