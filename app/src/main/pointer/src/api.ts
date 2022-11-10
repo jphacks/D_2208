06dddf92ab199e6ba37739281e4713ff1fbbce79
ygkn <2000ygkn0713@gmail.com>
2022-11-10 12:52:59 +0900

    join room with GraphQL instead of REST

--- app/src/main/pointer/src/api.ts
+++ app/src/main/pointer/src/api.ts
@@ -1,16 +1,60 @@
-import axios from "axios";
+import type { TypedDocumentNode } from "@graphql-typed-document-node/core";
+import { ExecutionResult, print } from "graphql";
+import { GraphQLClient } from "graphql-request";
+import { RemoveIndex, RequestOptions } from "graphql-request/dist/types";
+import { createClient, Sink } from "graphql-ws";
 
-import { Configuration, RoomApi } from "./generated/http-client";
+const graphqlHttpEndpoint = `${location.origin}/graphql`;
 
-const API_URL = location.origin;
+const graphqlHttpClient = new GraphQLClient(graphqlHttpEndpoint);
 
-const config = new Configuration({
-  basePath: API_URL,
+export const request = <
+  T = unknown,
+  V extends Record<string, unknown> = Record<string, unknown>
+>({
+  query,
+  variables,
+}: {
+  query: TypedDocumentNode<T, V>;
+} & (V extends Record<string | number | symbol, never>
+  ? { variables?: V }
+  : keyof RemoveIndex<V> extends never
+  ? { variables?: V }
+  : { variables: V })): Promise<T> => {
+  return graphqlHttpClient.request<T, V>({
+    document: query,
+    variables: variables,
+  } as RequestOptions<V, T>);
+};
+
+const graphqlWsEndpoint = `wss://${location.host}/graphql-ws`;
+
+const rawGraphqlWsClient = createClient({
+  url: graphqlWsEndpoint,
+  lazy: true,
 });
 
-const axiosInstance = axios.create({
-  baseURL: API_URL,
-  withCredentials: true,
-});
-
-export const roomApi = new RoomApi(config, "", axiosInstance);
+export const subscribe = <
+  T = unknown,
+  V extends Record<string, unknown> = Record<string, unknown>
+>(
+  {
+    query,
+    variables,
+  }: {
+    query: TypedDocumentNode<T, V>;
+  } & (V extends Record<string | number | symbol, never>
+    ? { variables?: V }
+    : keyof RemoveIndex<V> extends never
+    ? { variables?: V }
+    : { variables: V }),
+  sink: Sink<ExecutionResult<T>>
+) => {
+  rawGraphqlWsClient.subscribe(
+    {
+      query: print(query),
+      variables,
+    },
+    sink
+  );
+};
