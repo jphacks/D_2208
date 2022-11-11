9703cfe39a117acd07bb264c02c333e5826eda14
Hoshi Ran <ureshinostyle350@gmail.com>
2022-11-11 20:01:35 +0900

    remove: desktop > axios,OpenAPI,stomp を依存関係から削除した

--- desktop/packages/main/src/stomp.ts
+++ desktop/packages/main/src/stomp.ts
@@ -1,120 +1,124 @@
-import { Client, StompSubscription } from "@stomp/stompjs";
-import WebSocket from "ws";
+//
+// GraphQL 移行が完了すれば削除する
+//
 
-import { controller } from "./controller";
-import { goNext, goPrevious } from "./pagination";
+// import { Client, StompSubscription } from "@stomp/stompjs";
+// import WebSocket from "ws";
 
-Object.assign(global, { WebSocket });
+// import { controller } from "./controller";
+// import { goNext, goPrevious } from "./pagination";
 
-const brokerURL =
-  process.env["USE_DEV_BACKEND"] === "true"
-    ? "ws://localhost:8080/ws"
-    : "wss://smartpointer.abelab.dev/ws";
+// Object.assign(global, { WebSocket });
 
-const stompClient = new Client({
-  brokerURL,
-  debug: console.log,
-});
+// const brokerURL =
+//   process.env["USE_DEV_BACKEND"] === "true"
+//     ? "ws://localhost:8080/ws"
+//     : "wss://smartpointer.abelab.dev/ws";
 
-enum SlideControl {
-  NEXT,
-  PREVIOUS,
-}
+// const stompClient = new Client({
+//   brokerURL,
+//   debug: console.log,
+// });
 
-let slidesControlSubscription: StompSubscription | null = null;
-let pointerControlSubscription: StompSubscription | null = null;
+// enum SlideControl {
+//   NEXT,
+//   PREVIOUS,
+// }
 
-export const activate = () =>
-  new Promise<undefined>((resolve, reject) => {
-    if (stompClient.connected) {
-      resolve(undefined);
-      console.log("[STOMP] already connected");
-      return;
-    }
-    console.log(`[STOMP] Connecting to ${brokerURL}...`);
+// let slidesControlSubscription: StompSubscription | null = null;
+// let pointerControlSubscription: StompSubscription | null = null;
 
-    stompClient.onConnect = (frame) => {
-      console.log("[STOMP] Connected to broker:", frame);
-      resolve(undefined);
-    };
+// export const activate = () =>
+//   new Promise<undefined>((resolve, reject) => {
+//     if (stompClient.connected) {
+//       resolve(undefined);
+//       console.log("[STOMP] already connected");
+//       return;
+//     }
+//     console.log(`[STOMP] Connecting to ${brokerURL}...`);
 
-    stompClient.onStompError = (frame) => {
-      console.error("[STOMP] Broker reported error:");
-      console.error(frame.headers["message"]);
-      console.error("Additional details:");
-      console.error(frame.body);
-      reject(new Error(`STOMP error:\n${frame.headers["message"]}`));
-    };
+//     stompClient.onConnect = (frame) => {
+//       console.log("[STOMP] Connected to broker:", frame);
+//       resolve(undefined);
+//     };
 
-    stompClient.activate();
-  });
+//     stompClient.onStompError = (frame) => {
+//       console.error("[STOMP] Broker reported error:");
+//       console.error(frame.headers["message"]);
+//       console.error("Additional details:");
+//       console.error(frame.body);
+//       reject(new Error(`STOMP error:\n${frame.headers["message"]}`));
+//     };
 
-export const listenRoomSubscription = async (roomId: string) => {
-  slidesControlSubscription = stompClient.subscribe(
-    `/topic/rooms/${roomId}/slides/control`,
-    (message) => {
-      type Response = {
-        control: SlideControl;
-      };
-      const body = JSON.parse(message.body) as Response;
-      if (body.control === SlideControl.NEXT) {
-        goNext();
-      } else {
-        goPrevious();
-      }
-    }
-  );
+//     stompClient.activate();
+//   });
 
-  // ポインター操作を購読
-  pointerControlSubscription = stompClient.subscribe(
-    `/topic/rooms/${roomId}/pointer/control`,
-    (message) => {
-      type Response =
-        | {
-            isActive: true;
-            rotation: {
-              alpha: number;
-              beta: number;
-              gamma: number;
-            };
-          }
-        | {
-            isActive: false;
-            rotation: null;
-          };
-      const body = JSON.parse(message.body) as Response;
+// export const listenRoomSubscription = async (roomId: string) => {
+//   slidesControlSubscription = stompClient.subscribe(
+//     `/topic/rooms/${roomId}/slides/control`,
+//     (message) => {
+//       type Response = {
+//         control: SlideControl;
+//       };
+//       const body = JSON.parse(message.body) as Response;
+//       if (body.control === SlideControl.NEXT) {
+//         goNext();
+//       } else {
+//         goPrevious();
+//       }
+//     }
+//   );
 
-      if (body.isActive) {
-        // TODO: ポインターに認証情報が実装されたら、認証情報を使ってユーザーを特定する
-        controller.pointerUpdated(
-          {
-            id: "user-id",
-            name: "user-name",
-          },
-          {
-            alpha: body.rotation.alpha,
-            beta: body.rotation.beta,
-            gamma: body.rotation.gamma,
-          }
-        );
-      } else {
-        // TODO: ポインターに認証情報が実装されたら、認証情報を使ってユーザーを特定する
-        controller.pointerDeactivated({
-          id: "user-id",
-          name: "user-name",
-        });
-      }
-    }
-  );
-};
+//   // ポインター操作を購読
+//   pointerControlSubscription = stompClient.subscribe(
+//     `/topic/rooms/${roomId}/pointer/control`,
+//     (message) => {
+//       type Response =
+//         | {
+//             isActive: true;
+//             rotation: {
+//               alpha: number;
+//               beta: number;
+//               gamma: number;
+//             };
+//           }
+//         | {
+//             isActive: false;
+//             rotation: null;
+//           };
+//       const body = JSON.parse(message.body) as Response;
 
-export const unsubscribeRoomSubscription = () => {
-  if (slidesControlSubscription) {
-    slidesControlSubscription.unsubscribe();
-    slidesControlSubscription = null;
-  }
-  if (pointerControlSubscription) {
-    pointerControlSubscription.unsubscribe();
-    pointerControlSubscription = null;
-  }
-};
+//       if (body.isActive) {
+//         // TODO: ポインターに認証情報が実装されたら、認証情報を使ってユーザーを特定する
+//         controller.pointerUpdated(
+//           {
+//             id: "user-id",
+//             name: "user-name",
+//           },
+//           {
+//             alpha: body.rotation.alpha,
+//             beta: body.rotation.beta,
+//             gamma: body.rotation.gamma,
+//           }
+//         );
+//       } else {
+//         // TODO: ポインターに認証情報が実装されたら、認証情報を使ってユーザーを特定する
+//         controller.pointerDeactivated({
+//           id: "user-id",
+//           name: "user-name",
+//         });
+//       }
+//     }
+//   );
+// };
+
+// export const unsubscribeRoomSubscription = () => {
+//   if (slidesControlSubscription) {
+//     slidesControlSubscription.unsubscribe();
+//     slidesControlSubscription = null;
+//   }
+//   if (pointerControlSubscription) {
+//     pointerControlSubscription.unsubscribe();
+//     pointerControlSubscription = null;
+//   }
+// };
